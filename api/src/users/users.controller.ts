557638965cbd20fc385ff3545b048327eeba8a2f
Jerry <2593517388@qq.com>
2026-05-19 21:56:07 +0800

    feat: 添加用户更新功能及相关DTO和异常处理

--- api/src/users/users.controller.ts
+++ api/src/users/users.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Patch, Req, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { UsersService } from './users.service';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 interface AuthenticatedRequest extends Request {
   user: {
@@ -9,13 +11,27 @@ interface AuthenticatedRequest extends Request {
   };
 }
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get('me')
   @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('access-token')
+  @ApiOperation({ summary: '获取当前登录用户信息' })
   getMe(@Req() req: AuthenticatedRequest) {
     return this.usersService.getCurrentUser(req.user.userId);
+  }
+
+  @Patch('me')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth('access-token')
+  @ApiOperation({ summary: '更新当前登录用户信息' })
+  updateMe(
+    @Req() req: AuthenticatedRequest,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return this.usersService.updateCurrentUser(req.user.userId, updateUserDto);
   }
 }
--- api/src/users/users.service.ts
+++ api/src/users/users.service.ts
@@ -1,10 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(private readonly prismaService: PrismaService) {}
 
+  // 查询用户信息
   async getCurrentUser(userId: number) {
     const user = await this.prismaService.user.findUnique({
       where: {
@@ -13,9 +15,31 @@ export class UsersService {
     });
 
     if (!user) {
-      throw new Error('User not found');
+      throw new NotFoundException('用户不存在');
     }
 
     return user;
+  }
+
+  //   更新用户信息
+  async updateCurrentUser(userId: number, UpdateUserDto: UpdateUserDto) {
+    const existingUser = await this.prismaService.user.findUnique({
+      where: {
+        id: userId,
+      },
+    });
+
+    if (!existingUser) {
+      throw new NotFoundException('用户不存在');
+    }
+
+    const updateUser = await this.prismaService.user.update({
+      where: {
+        id: userId,
+      },
+      data: UpdateUserDto,
+    });
+
+    return updateUser;
   }
 }
